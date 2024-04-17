bd8cc30f1c826ad9304946657b6550150fcef623
Bennett Smrdel <bennett@midwestdiesel.com>
2024-04-17 13:01:07 -0500

    feat: Initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,21 @@
+import path from 'path';
+import webpack from 'webpack';
+import 'webpack-dev-server';
+
+const config: webpack.Configuration = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
+
+export default config;
