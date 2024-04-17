f372a3f87404da2c1c17f3ce7f1ce39ffe72a35b
Bennett Smrdel <bennett@midwestdiesel.com>
2024-04-17 13:38:20 -0500

    feat: Add scss

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-function component() {
+import "./styles/styles.scss";
+
+function main() {
   const element = document.createElement('div');
   element.innerHTML = 'Hello World!'
   return element;
 }
 
-document.body.appendChild(component());
+document.body.appendChild(main());
--- webpack.config.ts
+++ webpack.config.ts
@@ -9,12 +9,30 @@ const config: webpack.Configuration = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
     },
     compress: true,
     port: 9000,
+    proxy: [
+      {
+        context: ['/api'],
+        target: 'http://localhost:8002',
+      },
+    ],
   },
 };
 
